680d6fa453107b42f436676860445cc9ccbac670
MTH3 <dutra.md@hotmail.com>
2020-10-10 19:14:35 -0300

    inicio da tela de Cadastro criada, 80% concluida

--- lib/telas/telas_usuarios/decisao_usario.dart
+++ lib/telas/telas_usuarios/decisao_usario.dart
@@ -1,9 +1,16 @@
 import 'package:app_estetica/custom_widgets/buttons_widgets/custom_action_button.dart';
 import 'package:app_estetica/custom_widgets/text_widgets/custom_text.dart';
+import 'package:app_estetica/telas/telas_usuarios/tela_login.dart';
+import 'package:app_estetica/utils/nav.dart';
 import 'package:app_estetica/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
-class DecisaoUsuario extends StatelessWidget {
+class DecisaoUsuario extends StatefulWidget {
+  @override
+  _DecisaoUsuarioState createState() => _DecisaoUsuarioState();
+}
+
+class _DecisaoUsuarioState extends State<DecisaoUsuario> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -20,19 +27,24 @@ class DecisaoUsuario extends StatelessWidget {
           padding: EdgeInsets.only(top: 10, bottom: 20),
           child: CustomText(
             texto: 'Bem Vindo!',
+            cor: Colors.blue[600],
             tamanhoFonte: 40,
+            bold: true,
           ),
         ),
         CustomText(
           texto: 'Escolha como você deseja prosseguir usando o aplicativo',
           tamanhoFonte: 15,
+          cor: Colors.blue[600],
         ),
         Container(
           padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
           width: SizeConfig.blockSizeVertical * 65.0,
           child: CustomActionButton(
             campoNome: 'Usuario',
-            function: () {},
+            function: () {
+              push(context, TelaLogin());
+            },
           ),
         ),
         Container(
--- lib/telas/telas_usuarios/tela_login.dart
+++ lib/telas/telas_usuarios/tela_login.dart
@@ -46,7 +46,7 @@ class _TelaLoginState extends State<TelaLogin> {
                 acaoTeclado: TextInputAction.next,
                 tipoTeclado: TextInputType.emailAddress,
                 icone: Icon(
-                  Icons.person_outline,
+                  Icons.email_outlined,
                   color: Colors.black,
                 ),
                 backGColor: Colors.blue[100],
@@ -94,7 +94,7 @@ class _TelaLoginState extends State<TelaLogin> {
               padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
               width: SizeConfig.safeBlockVertical * 65.0,
               child: CustomActionButton(
-                campoNome: 'Loga',
+                campoNome: 'Logar',
                 function: () {},
               ),
             ),
