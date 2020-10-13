1891d0b2559f7d74dd764dc87cf8fffcbb30cc9d
MTH3 <dutra.md@hotmail.com>
2020-10-13 13:58:38 -0300

    algumas alteraçoes..

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:app_estetica/custom_widgets/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:core';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, widget),
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(480, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          ],
+          background: Container(color: Color(0xFFF5F5F5))),
       debugShowCheckedModeBanner: false,
       title: "App estética",
       theme: ThemeData(
--- lib/telas/telas_usuarios/decisao_usario.dart
+++ lib/telas/telas_usuarios/decisao_usario.dart
@@ -2,7 +2,6 @@ import 'package:app_estetica/custom_widgets/buttons_widgets/custom_action_button
 import 'package:app_estetica/custom_widgets/text_widgets/custom_text.dart';
 import 'package:app_estetica/telas/telas_usuarios/tela_login_cliente.dart';
 import 'package:app_estetica/utils/nav.dart';
-import 'package:app_estetica/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
 class DecisaoUsuario extends StatefulWidget {
@@ -13,7 +12,6 @@ class DecisaoUsuario extends StatefulWidget {
 class _DecisaoUsuarioState extends State<DecisaoUsuario> {
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
     return Scaffold(
       body: _body(),
     );
@@ -22,13 +20,6 @@ class _DecisaoUsuarioState extends State<DecisaoUsuario> {
   _body() {
     return Stack(
       children: [
-        Image.asset(
-          "assets/images/corte.jpg",
-          fit: BoxFit.cover,
-          height: double.infinity,
-          width: double.infinity,
-          alignment: Alignment.center,
-        ),
         Container(
           color: Colors.black45,
         ),
@@ -46,15 +37,12 @@ class _DecisaoUsuarioState extends State<DecisaoUsuario> {
             ),
             CustomText(
               texto: 'Como você deseja utilizar o aplicativo?',
-              tamanhoFonte: SizeConfig.safeBlockHorizontal * 4,
-              cor: Colors.white,
             ),
             Container(
               margin: EdgeInsets.only(
                 top: 5,
               ),
               padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-              width: SizeConfig.blockSizeVertical * 65.0,
               child: CustomActionButton(
                 campoNome: 'Cliente',
                 color: Colors.black54,
@@ -68,7 +56,6 @@ class _DecisaoUsuarioState extends State<DecisaoUsuario> {
             ),
             Container(
               padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-              width: SizeConfig.blockSizeVertical * 65.0,
               child: CustomActionButton(
                 campoNome: 'Gerente',
                 color: Colors.black54,
